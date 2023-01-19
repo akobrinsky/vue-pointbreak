520a0088bde51d7b3aec4c6854814551fe607092
Aryeh Kobrinsky <shmaryeh@gmail.com>
2023-01-19 15:44:41 -0500

    tests added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    setupFiles: resolve(__dirname, 'test/setup.ts')
+  }
 })
