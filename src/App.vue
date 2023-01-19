1758cd29a53074e2ea8cce0654dd89de8f351712
Aryeh Kobrinsky <shmaryeh@gmail.com>
2023-01-19 15:55:19 -0500

    wire up example app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import ActiveBreakpoints from './components/ActiveBreakpoints.vue';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="title">
+    pointbreak
+  </h1>
+  <ActiveBreakpoints />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.title {
+  color: #1abbcb;
+  text-shadow: 1px 1px 1px #038298;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import pointbreak from './pointbreak'
 
-createApp(App).mount('#app')
+createApp(App).use(pointbreak).mount('#app')
