a94b98baab89f401384e044590bf6de13a485429
Aryeh Kobrinsky <shmaryeh@gmail.com>
2023-01-25 10:36:38 -0500

    add width tracker to example app

--- src/components/ActiveBreakpoints.vue
+++ src/components/ActiveBreakpoints.vue
@@ -9,17 +9,31 @@
       {{ breakpoint }}
     </span>
   </div>
+  <div class="active-breakpoints">
+    <strong>Current width:</strong><span>{{ clientWidth }}</span>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { usePointbreak } from '../pointbreak';
 
 const pointbreak = usePointbreak();
 const breakpointTitleText = computed(() => (pointbreak.active ? 'Current Breakpoint: ' : 'Active Breakpoints: '));
+const clientWidth = ref<null | number>(null)
 
 const breakpointsShown = computed(() => {
   return Object.keys(pointbreak).filter((pb) => pointbreak[pb]);
+});
+
+const handleResize = (e: Event) => {
+  const target = e.target as Window
+  clientWidth.value = target.innerWidth
+};
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
+  clientWidth.value = window.innerWidth
 });
 
 </script>
@@ -30,5 +44,6 @@ const breakpointsShown = computed(() => {
 }
 .active-breakpoints {
   font-size: 1.5em;
+  margin-bottom: 0.5em;
 }
 </style>
