f2b1516222484d646a3697a96eff506e7cb67275
Aryeh Kobrinsky <shmaryeh@gmail.com>
2023-01-29 20:07:11 -0500

    ignore public folder in build

--- vite.config.ts
+++ vite.config.ts
@@ -14,24 +14,19 @@ export default defineConfig({
     },
   },
   build: {
+    copyPublicDir: false,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/lib/index.ts'),
       name: 'pointbreak',
-      // the proper extensions will be added
       fileName: 'index',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
         },
-        exports: "named"
+        exports: 'named'
       },
     },
   },
